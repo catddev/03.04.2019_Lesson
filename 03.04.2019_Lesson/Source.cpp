8aa127e9ba57b5dc6bc21845bde3f76b5e1be09e
айкеновад <@205_8>
2019-04-03 10:54:01 +0600

    -

--- 03.04.2019_Lesson/Source.cpp
+++ 03.04.2019_Lesson/Source.cpp
@@ -1,25 +1,18 @@
-#include"function.h"
-using namespace std;
-
-
-#define Capital
-#define KZ 1
-#define RU 2
-#define USA 3
+﻿#include"function.h"
 
 int cur_size = 0, buf_size = 0;
-void add(Country*&bs, Country el) {
+void add(Person*&bs, Person el) {
 	if (buf_size == 0)
 	{
 		buf_size = 4;
-		bs = new Country[buf_size];
+		bs = new Person[buf_size];
 	}
 	else
 	{
 		if (cur_size == buf_size)
 		{
 			buf_size *= 2;
-			Country*tmp1 = new Country[buf_size];
+			Person*tmp1 = new Person[buf_size];
 			for (int i = 0; i < cur_size; i++)
 				tmp1[i] = bs[i];
 			delete[] bs;
@@ -29,20 +22,80 @@ void add(Country*&bs, Country el) {
 	bs[cur_size++] = el;
 }
 
-
 int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 
-	Country *cs = 0;
-	Country tmp;
 	ifstream in_file("in.txt");
-	while (!in_file.eof())
+	Person *ps = 0;
+	Person tmp;
+	char str[100];
+	int y;
+	if (!in_file)
+		cerr << "File open error" << endl;
+	else
 	{
-		in_file >> tmp.title >> tmp.Cap.title >> tmp.Cap.population;
-		add(cs, tmp);
+		while (!in_file.eof())
+		{
+			in_file.getline(str, 100, '#');
+			tmp.setFull_name(str);
+
+			in_file.getline(str, 50, '#');
+			tmp.setAddress(str);
+
+			in_file >> str;
+			tmp.setGender(str);
+
+			in_file >> str;
+			tmp.setDegree(str);
+
+			in_file >> y;
+			tmp.setYear(y);
+
+			in_file.ignore();// !!
+
+			add(ps, tmp);
+		}
+
+		bool f = true;
+		while (f) {
+			int choice;
+			cout << " Enter 1 to print by year of birth" << endl;
+			cout << " Enter 2 to print by degree" << endl;
+			cout << " Enter 3 to print males only" << endl;
+			cout << " Enter 0 exit" << endl;
+			cin >> choice;
+			switch (choice)
+			{
+			case 1:
+				int n;
+				cout << "Enter the earliest year:" << endl;
+				cin >> n;
+				for (int i = 0; i < cur_size; i++)
+					if (ps[i].by_year(n))
+						ps[i].print();
+				cout << endl;
+				break;
+			case 2:
+				cout << "Enter a degree:" << endl;
+				cin.ignore();
+				cin.getline(str, 50);
+				for (int i = 0; i < cur_size; i++)
+					if (ps[i].by_degree(str))
+						ps[i].print();
+				cout << endl;
+				break;
+			case 3:
+				for (int i = 0; i < cur_size; i++)
+					if (ps[i].by_gender(str))
+						ps[i].print();
+				cout << endl;
+				break;
+			case 0:
+				f = false;
+				break;
+			}
+		}
 	}
-	cs[KZ].print();
-
-
-
 	system("pause");
 }
---
+++ 03.04.2019_Lesson/function.cpp
@@ -0,0 +1,93 @@
+﻿#include"function.h"
+
+Person::Person(const char*full_name, const char*gender) {
+	strcpy_s(this->full_name, full_name);
+	strcpy_s(this->gender, gender);
+}
+
+Person::Person(const char*full_name, const char *address, const char* gender, const char *degree, int year) {
+	strcpy_s(this->full_name, full_name);
+	strcpy_s(this->address, address);
+	strcpy_s(this->gender, gender);
+	strcpy_s(this->degree, degree);
+	this->year = year;
+}
+
+void Person::setFull_name(const char * full_name)
+{
+	strcpy_s(this->full_name, full_name);
+}
+
+void Person::setAddress(const char * address)
+{
+	strcpy_s(this->address, address);
+}
+
+void Person::setGender(const char * gender)
+{
+	strcpy_s(this->gender, gender);
+}
+
+void Person::setDegree(const char * degree)
+{
+	strcpy_s(this->degree, degree);
+}
+
+void Person::setYear(int year)
+{
+	this->year = year;
+}
+
+const char * Person::getFull_name()
+{
+	return full_name;
+}
+
+const char * Person::getAddress()
+{
+	return address;
+}
+
+const char * Person::getGender()
+{
+	return gender;
+}
+
+const char * Person::getDegree()
+{
+	return degree;
+}
+
+int Person::getYear()
+{
+	return year;
+}
+
+void Person::print()
+{
+	cout << full_name << ": " << address << endl;
+	cout << gender << "; " << degree << "; " << year << endl;
+}
+
+bool Person::by_year(int y)
+{
+	return (year > y);
+}
+
+bool Person::by_degree(const char * str)
+{
+	if (strcmp(degree, str) == 0)
+		return true;
+	else
+		return false;
+}
+
+bool Person::by_gender(const char * str)
+{
+	if (strcmp(gender, "мужской") == 0)
+		return true;
+	else
+		return false;
+}
+
+
